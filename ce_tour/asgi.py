61c6a6536587fa07490aceaf8bde707ef9e3d818
Mohammad Ferry Husnil Arif <ferryhusnil07@gmail.com>
2024-06-01 09:22:03 +0700

    chore: set up pre-commit hook, black and isort

--- ce_tour/asgi.py
+++ ce_tour/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ce_tour.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ce_tour.settings")
 
 application = get_asgi_application()
--- ce_tour/wsgi.py
+++ ce_tour/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ce_tour.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ce_tour.settings")
 
 application = get_wsgi_application()
