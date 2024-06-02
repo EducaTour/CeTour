9f18bf914d82c07d14835bbb21531aba6d753fb1
aptarr_ <apta.rasendriya@gmail.com>
2024-06-03 01:12:14 +0700

    feat: create initial models for prediction app

--- app/prediction/models.py
+++ app/prediction/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
+import os
 
-# Create your models here.
+def get_file_path(filename):
+    ext = filename.split('.')[-1]
+    new_filename = f"{uuid.uuid4()}.{ext}"
+    return os.path.join('photos/', new_filename)
+
+class Prediction(models.Model):
+    result = models.CharField(max_length=200)
+    rate = models.DecimalField(max_digits=4, decimal_places=2)
+    image = models.ImageField(upload_to=get_file_path)
+    createdAt = models.DateTimeField(auto_now_add=True)
--- app/prediction/urls.py
+++ app/prediction/urls.py
@@ -1,1 +1,8 @@
 urlpatterns = []
+from django.urls import path
+
+from .views import AddCapture
+
+urlpatterns = [
+    path("", AddCapture.as_view(), name="landmark"),
+]
