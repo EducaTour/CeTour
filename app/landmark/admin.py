d42bf8a1a66abb8f7efcde90f0ebcad26f71239e
Mohammad Ferry Husnil Arif <ferryhusnil07@gmail.com>
2024-06-03 06:48:44 +0700

    feat(admin): add display id for landmark

--- app/landmark/admin.py
+++ app/landmark/admin.py
@@ -9,8 +9,23 @@ from .models import (
     UniqueActivity,
 )
 
+
+# Custom admin class for Landmark
+class LandmarkAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "description",
+        "history",
+        "phone",
+        "email",
+        "website",
+        "location",
+    )
+
+
 # Register your models here.
-admin.site.register(Landmark)
+admin.site.register(Landmark, LandmarkAdmin)
 admin.site.register(Location)
 admin.site.register(OpeningHour)
 admin.site.register(PhotoLandmark)
