64b64d62c75e810854e6cef7e6e9b99ecb3141f6
aptarr_ <apta.rasendriya@gmail.com>
2024-06-16 22:06:27 +0700

    chore: update migrations

--- app/prediction/models.py
+++ app/prediction/models.py
@@ -5,4 +5,4 @@ class Prediction(models.Model):
     result = models.CharField(max_length=200)
     rate = models.DecimalField(max_digits=4, decimal_places=2)
     image = models.URLField()
-    createdAt = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
