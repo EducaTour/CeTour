c9cb759a9f426812a44de646c29935473f963992
aptarr_ <apta.rasendriya@gmail.com>
2024-06-20 17:09:40 +0700

    chore: update display admin

--- app/landmark/admin.py
+++ app/landmark/admin.py
@@ -34,12 +34,55 @@ class LandmarkContentAdmin(admin.ModelAdmin):
     )
 
 
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "address",
+        "location_url",
+        "landmark",
+    )
+
+
+class OpeningHourAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "open_time",
+        "close_time",
+        "landmark",
+    )
+
+
+class PhotoLandmarkAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "photo_url",
+        "landmark",
+    )
+
+
+class UniqueActivityAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "landmark",
+    )
+
+
+class TicketPriceAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "age",
+        "price",
+        "landmark",
+    )
+
+
 # Register your models here.
 admin.site.register(Landmark, LandmarkAdmin)
 admin.site.register(Language)
 admin.site.register(LandmarkContent, LandmarkContentAdmin)
-admin.site.register(Location)
-admin.site.register(OpeningHour)
-admin.site.register(PhotoLandmark)
-admin.site.register(TicketPrice)
-admin.site.register(UniqueActivity)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(OpeningHour, OpeningHourAdmin)
+admin.site.register(PhotoLandmark, PhotoLandmarkAdmin)
+admin.site.register(TicketPrice, TicketPriceAdmin)
+admin.site.register(UniqueActivity, UniqueActivityAdmin)
